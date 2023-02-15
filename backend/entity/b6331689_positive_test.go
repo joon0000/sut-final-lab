46cd6ef3ae9e1644d8834714840c1d2c44cb54c3
unknown <junekawin@icloud.com>
2023-02-15 10:12:18 -0800

    abc

---
+++ backend/entity/b6331689_positive_test.go
@@ -0,0 +1,24 @@
+package entity
+
+import(
+	"testing"
+	. "github.com/onsi/gomega"
+	. "github.com/asaskevich/govalidator"
+	. "github.com/joon0000/sut-final-lab"
+)
+
+func TestCustomerValidatePass (*t testing) {
+	g := Newgomegawith(t)
+	t.Run("pass case",func TestCustomerValidatePass (*t testing) {
+		c := Customer{
+			Name: "kawin",
+			Email: "kawin@gmail.com",
+			CustomerID: "L1234567",
+		}
+
+		ok,err := govalidator.ValidateStruct(c)
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).ToNot(BeNil())
+		g.Expect(err.Error).Error.Equal("pass case")
+	})
+}
