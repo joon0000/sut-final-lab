435d5203ece3bfdb296fd6e646db31c44e828187
unknown <junekawin@icloud.com>
2023-02-15 10:22:37 -0800

    update negative name - close #2

--- backend/entity/b6331689_negative_name_test.go
+++ backend/entity/b6331689_negative_name_test.go
@@ -1,5 +1,24 @@
 package entity
 
-import (
+import(
 	"testing"
+	. "github.com/onsi/gomega"
+	. "github.com/asaskevich/govalidator"
+	. "github.com/joon0000/sut-final-lab"
 )
+
+func TestCustomerValidatePass (*t testing) {
+	g := Newgomegawith(t)
+	t.Run("check name not blank",func TestCustomerValidatePass (*t testing) {
+		c := Customer{
+			Name: "",
+			Email: "kawin@gmail.com",
+			CustomerID: "L7654321",
+		}
+
+		ok,err := govalidator.ValidateStruct(c)
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).ToNot(BeNil())
+		g.Expect(err.Error).Equal("name not blank")
+	})
+}
